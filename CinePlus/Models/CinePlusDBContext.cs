a1ca4766a8ae5a82620f151829659767bbf420a7
Arnel Sánchez Rodríguez <arnelsanchezrodriguez@gmail.com>
2021-05-16 14:18:43 -0400

    Actualizar la Base de Datos2

--- CinePlus/Models/CinePlusDBContext.cs
+++ CinePlus/Models/CinePlusDBContext.cs
@@ -13,6 +13,26 @@ namespace CinePlus.Models
 
         public DbSet<User> User { get; set; }
 
+        public DbSet<ArmChair> ArmChair { get; set; }
+
+        public DbSet<Discount> Discount { get; set; }
+
+        public DbSet<DiscountsByShow> DiscountsByShow { get; set; }
+
+        public DbSet<Movie> Movie { get; set; }
+
+        public DbSet<MovieOnTop10> MovieOnTop10 { get; set; }
+
+        public DbSet<MovieType> MovieType { get; set; }
+
         public DbSet<Partner> Partner { get; set; }
+
+        public DbSet<Room> Room { get; set; }
+
+        public DbSet<Show> Show { get; set; }
+
+        public DbSet<Top10> Top10 { get; set; }
+
+        public DbSet<UserBoughtArmChair> UserBoughtArmChair { get; set; }
     }
 }
